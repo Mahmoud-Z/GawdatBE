21ac09e84b2f745d1312a1542df01e4d4c729ab4
Mahmoud <mahmoudgamal7575@act.eg>
2023-06-10 00:20:31 +0300

    All Fixes

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,24 +5,24 @@ const e = require('cors');
 const sql = require('mssql')//call for using sql module
 let mssql = require('../configuration/mssql-pool-management.js')
 const config = require('../Configuration/config')//call for using configuration module that we create it to store database conaction
- const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 const { password } = require('../Configuration/config');
 async function startTimer(newTask,id){
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
-    let newEndDate=new Date()
-    newEndDate.setDate(newEndDate.getDate() - parseInt(newTask.duration.split(':')[0]))
-    newEndDate.setHours(newEndDate.getHours() - parseInt(newTask.duration.split(':')[1]))
-    newEndDate.setMinutes(newEndDate.getMinutes() - parseInt(newTask.duration.split(':')[2]))
-    newEndDate.setSeconds(newEndDate.getSeconds() - parseInt(newTask.duration.split(':')[3]))
-    console.log(`UPDATE [dbo].[Task] SET [endDate]='${new Date(newEndDate).toISOString()}',[status]='true' WHERE id=${newTask.id}`);
-    await request.query(`UPDATE [dbo].[Task] SET [endDate]='${new Date(newEndDate).toISOString()}',[status]='true' WHERE id=${newTask.id}`);
+    let newstartDate=new Date()
+    newstartDate.setDate(newstartDate.getDate() - parseInt(newTask.duration.split(':')[0]))
+    newstartDate.setHours(newstartDate.getHours() - parseInt(newTask.duration.split(':')[1]))
+    newstartDate.setMinutes(newstartDate.getMinutes() - parseInt(newTask.duration.split(':')[2]))
+    newstartDate.setSeconds(newstartDate.getSeconds() - parseInt(newTask.duration.split(':')[3]))
+    //console.log(`UPDATE [dbo].[Task] SET [startDate]='${new Date(newstartDate).toISOString()}',[status]='true' WHERE id=${newTask.id}`);
+    await request.query(`UPDATE [dbo].[Task] SET [startDate]='${new Date(newstartDate).toISOString()}',[status]='true' WHERE id=${newTask.id}`);
 }
 async function pauseTimer(oldTask,id){
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)  
-    var seconds = Math.floor(((new Date().getTime()) - (new Date(oldTask.endDate).getTime()))/1000);
+    var seconds = Math.floor(((new Date().getTime()) - (new Date(oldTask.startDate).getTime()))/1000);
     var minutes = Math.floor(seconds/60);
     var hours = Math.floor(minutes/60);
     var days = Math.floor(hours/24);
@@ -44,10 +44,14 @@ module.exports.importMachine = async (req, res) => {
 
     await request.query(`
     INSERT INTO [dbo].[Machine]
-               ([name]
+               ([name],
+                [status],
+                productivity
                )
          VALUES
                ('${req.body.machineName}'
+               ,1
+               ,'${req.body.productivity}'
                )
     `);
     
@@ -78,7 +82,7 @@ module.exports.getPermissionResponse = async (req, res) => {
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
     let data = await request.query(`select * from permissionResponse where username='${req.body.username}'`);
-    console.log(`select * from permissionResponse where username='${req.body.username}'`);
+    //console.log(`select * from permissionResponse where username='${req.body.username}'`);
     res.json(data.recordset)
 }
 module.exports.addPermission = async (req, res) => {
@@ -111,9 +115,9 @@ module.exports.checkUser = async (req, res) => {
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
     try {
-        console.log(`select userName from User where userName='${req.body.userName}'`);
+        //console.log(`select userName from User where userName='${req.body.userName}'`);
         let data=await request.query(`select userName from users where userName='${req.body.userName}'`);
-        console.log(data.recordset);
+        //console.log(data.recordset);
         res.json(data.recordset)
 
     } catch (error) {
@@ -124,9 +128,7 @@ module.exports.checkUser = async (req, res) => {
 module.exports.addUser = async (req, res) => {
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
-    bcrypt.hash(req.body.password,8, async function (err, hash) {
-
-    
+    //bcrypt.hash(req.body.password,8, async function (err, hash) {
     await request.query(`
 
     INSERT INTO [dbo].[users]
@@ -139,7 +141,7 @@ module.exports.addUser = async (req, res) => {
     ,[permissionCheckBox])
 VALUES
     ('${req.body.userName}'
-    ,'${hash}'
+    ,'${password}'
     ,'${req.body.checkbox0}'
     ,'${req.body.checkbox1}'
     ,'${req.body.checkbox2}'
@@ -147,7 +149,7 @@ VALUES
     ,'${req.body.checkbox4}')
     `);
     
-})
+
     res.json('inserted successfully')
 };
 module.exports.importTasks = async (req, res) => {
@@ -156,7 +158,8 @@ module.exports.importTasks = async (req, res) => {
     let date=new Date().getTime();
     let tasksOrder;
     date += ((req.body.TaskDurationH*60*60*1000)+(req.body.TaskDurationM*60*1000))
-
+    let minMachineId=await (await request.query(`select min(id) from machine`)).recordset[0][""]
+    console.log("req body " + req.body.MachinePath);
     await request.query(`
     INSERT INTO [dbo].[Task]
     ([customerName]
@@ -181,7 +184,7 @@ module.exports.importTasks = async (req, res) => {
        ,[stepCode]
        ,[stepName]
        ,[stepFactor]
-       ,[endDate]
+       ,[startDate]
        ,[machineId]
        ,[machinePath]
        ,[status]
@@ -212,20 +215,19 @@ module.exports.importTasks = async (req, res) => {
                '${req.body.StepName}',
                '${req.body.StepFactor}',
                '${new Date().toISOString()}',
-               '9',
+               '${minMachineId}',
                '${req.body.MachinePath}',
                'false',
                '0:0:0:0'
               )
     `);
     let taskId=await (await request.query(`select max(id) from Task`)).recordset[0][""]
-
-    if (await (await request.query(`select taskNumber from Machine where id=9`)).recordset[0].taskNumber==null || await (await request.query(`select taskNumber from Machine where id=9`)).recordset[0].taskNumber=='') 
+    if (await (await request.query(`select taskNumber from Machine where id=${minMachineId}`)).recordset[0].taskNumber==null || await (await request.query(`select taskNumber from Machine where id=${minMachineId}`)).recordset[0].taskNumber=='') 
         tasksOrder=taskId
     else
 
-        tasksOrder=await (await request.query(`select taskNumber from Machine where id=9`)).recordset[0].taskNumber+","+taskId
-    await request.query(`UPDATE [dbo].[Machine] SET [taskNumber]='${tasksOrder}' WHERE id=9`);
+        tasksOrder=await (await request.query(`select taskNumber from Machine where id=${minMachineId}`)).recordset[0].taskNumber+","+taskId
+    await request.query(`UPDATE [dbo].[Machine] SET [taskNumber]='${tasksOrder}' WHERE id=${minMachineId}`);
     res.json('inserted successfully')
 }
 module.exports.editOrder = async (req, res) => {
@@ -308,6 +310,7 @@ module.exports.getMachine = async (req, res) => {
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
     let data=await request.query(`select * from Machine`);
+    console.log(data.recordset)
     res.json(data.recordset)
 }
 module.exports.getCustomers = async (req, res) => {
@@ -362,7 +365,7 @@ module.exports.getPermissions = async (req, res) => {
     res.json(data.recordset)
 }
 module.exports.deleteMachine = async (req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
     await request.query(`delete from Machine where id=${req.body.id}
@@ -372,18 +375,18 @@ module.exports.deleteMachine = async (req, res) => {
 module.exports.deleteTask = async (req, res) => {
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
-    console.log(await (await request.query(`select machineId from Task where id=${req.body.id}`)));
+    //console.log(await (await request.query(`select machineId from Task where id=${req.body.id}`)));
     let mid =await (await request.query(`select machineId from Task where id=${req.body.id}`)).recordset[0].machineId
     let tasksOrder=await (await request.query(`select taskNumber from Machine where id=${mid}`)).recordset[0].taskNumber
-    console.log(tasksOrder,tasksOrder.split(','),'before');
-    console.log(tasksOrder.indexOf(req.body.id));
+    //console.log(tasksOrder,tasksOrder.split(','),'before');
+    //console.log(tasksOrder.indexOf(req.body.id));
     let newIds=[];
     for (let i = 0; i < tasksOrder.split(',').length; i++) {
         if (tasksOrder.split(',')[i]!=req.body.id) {
             newIds.push(tasksOrder.split(',')[i])
         }
     }
-    console.log(newIds.join(','));
+    //console.log(newIds.join(','));
     await request.query(`UPDATE [dbo].[Machine] SET [taskNumber]='${newIds.join(',')}' WHERE id=${mid}`);
     await request.query(`delete from Task where id=${req.body.id}`);
     res.json('Deleted successfully')
@@ -391,7 +394,7 @@ module.exports.deleteTask = async (req, res) => {
 module.exports.updateTask = async (req, res) => {
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
-    console.log(req.body);
+    //console.log(req.body);
     for (let i = 0; i < req.body.taskId.length; i++) {
         await request.query(`UPDATE [dbo].[Task] SET [machineId]=${req.body.machineId} WHERE id=${req.body.taskId[i]}`);
     }
@@ -402,7 +405,7 @@ module.exports.updateTask = async (req, res) => {
 module.exports.changeTime = async (req, res) => {
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
-    console.log(req.body);
+    //console.log(req.body);
     if (req.body.previousContainer==req.body.container) {
         if (req.body.previousIndex!=req.body.currentIndex) {
             let machineTaskId=await (await request.query(`select taskNumber from Machine where id=${req.body.container}`)).recordset[0];
@@ -416,54 +419,54 @@ module.exports.changeTime = async (req, res) => {
         let machineOldTaskId=await (await request.query(`select taskNumber from Machine where id=${req.body.previousContainer}`)).recordset[0];
         let machineNewTaskId=await (await request.query(`select taskNumber from Machine where id=${req.body.container}`)).recordset[0];
         if (req.body.previousIndex==0&&req.body.currentIndex==0) {
-            console.log("0=>0");
+            //console.log("0=>0");
             let oldTask;
             let newTask;
-            console.log(machineOldTaskId.taskNumber.split(','));
-            console.log(machineNewTaskId.taskNumber.split(','));
+            //console.log(machineOldTaskId.taskNumber.split(','));
+            //console.log(machineNewTaskId.taskNumber.split(','));
             if (machineOldTaskId.taskNumber.split(',').length>1){
-                console.log("Started");
-                console.log(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[1]}`);
+                //console.log("Started");
+                //console.log(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[1]}`);
                 oldTask=await (await request.query(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[1]}`)).recordset[0];
                 startTimer(oldTask)
             }
             if (machineNewTaskId.taskNumber.split(',').length>0&&machineNewTaskId.taskNumber.split(',')[0]!=""){
-                console.log("Paused");
-                console.log(`select * from Task where id=${machineNewTaskId.taskNumber.split(',')[0]}`);
+                //console.log("Paused");
+                //console.log(`select * from Task where id=${machineNewTaskId.taskNumber.split(',')[0]}`);
                 newTask=await (await request.query(`select * from Task where id=${machineNewTaskId.taskNumber.split(',')[0]}`)).recordset[0];
                 pauseTimer(newTask)
             }
         }
         else if(req.body.previousIndex!=0&&req.body.currentIndex==0){
-            console.log("!0=>0");
+            //console.log("!0=>0");
             let oldTask;
             let newTask;
-            console.log(machineOldTaskId.taskNumber.split(','));
-            console.log(machineNewTaskId.taskNumber.split(','));
-            console.log(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[req.body.previousIndex]}`);
+            //console.log(machineOldTaskId.taskNumber.split(','));
+            //console.log(machineNewTaskId.taskNumber.split(','));
+            //console.log(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[req.body.previousIndex]}`);
             oldTask=await (await request.query(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[req.body.previousIndex]}`)).recordset[0];
             startTimer(oldTask)
             if (machineNewTaskId.taskNumber.split(',').length>0&&machineNewTaskId.taskNumber.split(',')[0]!=""){
-                console.log(`select * from Task where id=${machineNewTaskId.taskNumber.split(',')[0]}`);
+                //console.log(`select * from Task where id=${machineNewTaskId.taskNumber.split(',')[0]}`);
                 newTask=await (await request.query(`select * from Task where id=${machineNewTaskId.taskNumber.split(',')[0]}`)).recordset[0];
                 pauseTimer(newTask)
             }
         }
         else if(req.body.previousIndex==0&&req.body.currentIndex!=0){
-            console.log("0=>!0");
+            //console.log("0=>!0");
             let oldTask;
             let newTask;
-            console.log(machineOldTaskId.taskNumber.split(','));
-            console.log(machineNewTaskId.taskNumber.split(','));
+            //console.log(machineOldTaskId.taskNumber.split(','));
+            //console.log(machineNewTaskId.taskNumber.split(','));
             if (machineOldTaskId.taskNumber.split(',').length>1){
-                console.log(`Started`);
-                console.log(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[1]}`);
+                //console.log(`Started`);
+                //console.log(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[1]}`);
                 oldTask=await (await request.query(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[1]}`)).recordset[0];
                 startTimer(oldTask)
             }
             if (machineNewTaskId.taskNumber.split(',').length>0&&machineNewTaskId.taskNumber.split(',')[0]!=""){
-                console.log(`Pasued`);
-                console.log(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[0]}`);
+                //console.log(`Pasued`);
+                //console.log(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[0]}`);
                 newTask=await (await request.query(`select * from Task where id=${machineOldTaskId.taskNumber.split(',')[0]}`)).recordset[0];
                 pauseTimer(newTask)
             }
@@ -472,7 +475,7 @@ module.exports.changeTime = async (req, res) => {
     res.json('Deleted successfully')
 }
 module.exports.logIn= async (req, res) => {
-                     console.log(req.body);
+                     //console.log(req.body);
     const { email, password } = req.body;
     //db connection
     let dbconfig = await sql.connect(config)
@@ -487,9 +490,9 @@ module.exports.logIn= async (req, res) => {
     
      else {
       
-        let match = await bcrypt.compare(password,user.recordset[0].password.trim())
+    //    let match = await bcrypt.compare(password,user.recordset[0].password.trim())
 
-     if (match) {
+    //   if (match) {
         var token = jwt.sign({ user:user.recordset }, 'gowdat');
         
         
@@ -498,12 +501,12 @@ module.exports.logIn= async (req, res) => {
        
       }
 
-       else {
-        res.json("WRONG PASSWORD")
+    //   else {
+    //     res.json("WRONG PASSWORD")
 
-      }
+    //   }
 
-     }
+    // }
     
 
 }
@@ -534,7 +537,7 @@ module.exports.startTask = async (req, res) => {
 module.exports.stopTask = async (req, res) => {
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
-    console.log('sadsadsa');
+    //console.log('sadsadsa');
     let oldTask=await (await request.query(`select * from Task where id=${req.body.id}`)).recordset[0];
     pauseTimer(oldTask,req.body.id)
     await request.query(`UPDATE [dbo].[Task] SET [status]='false' WHERE id=${req.body.id}`);
@@ -559,7 +562,7 @@ module.exports.stopMachine = async (req, res) => {
     }
     else{
         let task=await (await request.query(`select * from task where id in (${machine.taskNumber})`)).recordset;
-        console.log(task.length);
+        //console.log(task.length);
         for (let i = 0; i < task.length; i++) {
             pauseTimer(task[i],task[i].id)
         }
@@ -596,26 +599,10 @@ module.exports.addOrder = async (req, res) => {
     await request.query(`UPDATE [dbo].[Permission] SET [submitted]='FALSE' WHERE permissionId=${req.body.permissionId}`);
     res.json('Permission is deleted')
 }
-
-module.exports.MachinesReport = async (req, res) => {
+module.exports.finishTask = async (req, res) => {
     let sqlPool = await mssql.GetCreateIfNotExistPool(config)
     let request = new sql.Request(sqlPool)
-    await request.query(`select  [dbo].[Permission] SET [submitted]='FALSE' WHERE permissionId=${req.body.permissionId}`);
-    res.json('Permission is deleted')
-}
-module.exports.TotalMachinesReport = async (req, res) => {
-    let sqlPool = await mssql.GetCreateIfNotExistPool(config)
-    let request = new sql.Request(sqlPool)
-   var TotalMachinesReport = await request.query(` SELECT   SUM(len(Machine.taskNumber) - len(replace(Machine.taskNumber, ',', '')) +1) AS numOfTasks, SUM(Task.orderSheets) 
-    AS numOfSheets , SUM(Task.orderTotalAmount) AS TOTAL FROM Task  
-	left JOIN Machine on  Task.id in(SELECT value FROM STRING_SPLIT(Machine.taskNumber, ','))`);
-    console.log(TotalMachinesReport.recordset[0]);
-    res.json(TotalMachinesReport.recordset)
-}
-module.exports.TaskReport = async (req, res) => {
-    let sqlPool = await mssql.GetCreateIfNotExistPool(config)
-    let request = new sql.Request(sqlPool)
-    await request.query(`UPDATE [dbo].[Permission] SET [submitted]='FALSE' WHERE permissionId=${req.body.permissionId}`);
+    await request.query(`UPDATE [dbo].[Task] SET [endDate]='${new Date().toISOString()}',[orderStatus] = 'Done',machineId = '',status = 'false' WHERE id=${req.body.id}`);
     res.json('Permission is deleted')
 }
 // jwt.verify(token,'gowdat',(err,decodded)=>{
@@ -623,8 +610,8 @@ module.exports.TaskReport = async (req, res) => {
 //         res.json('err')
 //     }
 //     else {
-//  console.log(decodded)
+//  //console.log(decodded)
 //          res.json({message : 'success'})
 //     }
-//     console.log(decodded)
+//     //console.log(decodded)
 // })
